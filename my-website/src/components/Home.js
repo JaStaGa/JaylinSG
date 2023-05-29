31cf50af04c7baaa0f6e2d4e95a0275456912b5c
JaStaGa <jaylinstarlinganaway@gmail.com>
2023-05-29 00:04:03 -0400

    edit home page

--- my-website/src/components/Home.js
+++ my-website/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      {/* Add your content here */}
+    <div className="container">
+      <h1 className="heading">Welcome to My Website</h1>
+      <p className="description">Explore and discover amazing things</p>
+      {/* Add your additional content here */}
     </div>
   );
 }
